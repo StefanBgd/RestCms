21667bd9465ec2f016cd1f43b1d26d8527f0ce16
Stefan Stanojevic <mrstefan1100@gmail.com>
2015-08-26 13:45:58 +0200

    inicijalni projekat

---
+++ src/main/java/rs/htec/cmsrest/cms/News.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.htec.cmsrest.cms;
+
+/**
+ *
+ * @author stefan
+ */
+public class News {
+
+    private int id;
+    private String title;
+    private String body;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
